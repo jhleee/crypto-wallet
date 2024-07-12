9bf4dcb70e5a9d6496ef9e5fb60452a4901c31f9
jhleee <ng0301@gmail.com>
2024-07-12 14:05:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/crypto-wallet/", // GitHub 저장소 이름으로 수정
+    base: "/v0-app", // GitHub 저장소 이름으로 수정
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
